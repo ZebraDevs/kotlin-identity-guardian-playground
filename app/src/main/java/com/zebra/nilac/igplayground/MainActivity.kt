c6489399309556b17110365988c9ce6018a445b4
Calin Neamtu <nilac8991@gmail.com>
2024-03-06 16:49:12 +0100

    Initial source code bring up
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

---
+++ app/src/main/java/com/zebra/nilac/igplayground/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.zebra.nilac.igplayground
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.zebra.nilac.igplayground.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
