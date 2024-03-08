69426dfa1307fa1a5cd823f48f532066ad5814aa
Calin Neamtu <nilac8991@gmail.com>
2024-03-08 11:48:45 +0100

    Add support to retrieve previous user session
    Signed-off-by: Calin Neamtu <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/nilac/igplayground/MainActivity.kt
+++ app/src/main/java/com/zebra/nilac/igplayground/MainActivity.kt
@@ -25,13 +25,22 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        binding.currentSessionButon.setOnClickListener {
+        binding.currentSessionBtn.setOnClickListener {
             if (!EMDKLoader.getInstance().isManagerInit()) {
                 Toast.makeText(this, "EMDK Manager is not yet initialized!", Toast.LENGTH_LONG)
                     .show()
                 return@setOnClickListener
             }
             getCurrentUserSession()
+        }
+
+        binding.previousSessionBtn.setOnClickListener {
+            if (!EMDKLoader.getInstance().isManagerInit()) {
+                Toast.makeText(this, "EMDK Manager is not yet initialized!", Toast.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            }
+            getPreviousUserSession()
         }
 
         initEMDKManager()
@@ -53,7 +62,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getCurrentUserSession() {
-        var currentSession = ""
+        var userSession = ""
 
         contentResolver.query(
             Uri.parse(CURRENT_SESSION_URI),
@@ -69,10 +78,33 @@ class MainActivity : AppCompatActivity() {
 
             it.moveToNext()
             for (i in 0 until it.columnCount) {
-                currentSession = "$currentSession\n${it.getColumnName(i)}: ${it.getString(i)}\n"
+                userSession = "$userSession\n${it.getColumnName(i)}: ${it.getString(i)}\n"
             }
         }
-        binding.userSession.text = currentSession
+        binding.userSession.text = userSession
+    }
+
+    private fun getPreviousUserSession() {
+        var userSession = ""
+
+        contentResolver.query(
+            Uri.parse(PREVIOUS_SESSION_URI),
+            null,
+            null,
+            null
+        ).use {
+            if (it == null || it.columnCount == 0) {
+                Log.e(TAG, "App is not having permission for this API")
+                acquirePermissionForPreviousUserSession()
+                return
+            }
+
+            it.moveToNext()
+            for (i in 0 until it.columnCount) {
+                userSession = "$userSession\n${it.getColumnName(i)}: ${it.getString(i)}\n"
+            }
+        }
+        binding.userSession.text = userSession
     }
 
     private fun acquirePermissionForUserSession() {
@@ -95,10 +127,32 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
+    private fun acquirePermissionForPreviousUserSession() {
+        Log.i(TAG, "Acquiring permission to check the previous user session")
+        ProfileLoader().processProfile(
+            "IGPreviousSession",
+            null,
+            object : ProfileLoaderResultCallback {
+                override fun onProfileLoadFailed(errorObject: EMDKResults) {
+                    //Nothing to see here..
+                }
+
+                override fun onProfileLoadFailed(message: String) {
+                    Log.e(TAG, "Failed to process profile")
+                }
+
+                override fun onProfileLoaded() {
+                    getCurrentUserSession()
+                }
+            })
+    }
+
     companion object {
         private const val TAG = "MainActivity"
 
         private const val CURRENT_SESSION_URI =
             "content://com.zebra.mdna.els.provider/currentsession"
+        private const val PREVIOUS_SESSION_URI =
+            "content://com.zebra.mdna.els.provider/previoussession"
     }
 }
